0b0d48430119cd6e59e4627e8430c85bf76f227c
wilgur513 <wilgur513@naver.com>
2022-02-28 15:33:53 +0900

    refactor: 정적 메서드 상수로 변경

--- src/main/java/lotto/view/StringFormatValidator.java
+++ src/main/java/lotto/view/StringFormatValidator.java
@@ -2,6 +2,8 @@ package lotto.view;
 
 public class StringFormatValidator {
 
+    private static final String NUMBER_REGEX = "[1-9][0-9]*";
+    private static final String NUMBER_WITH_SPACE_REGEX = "\\s*" + NUMBER_REGEX + "\\s*";
     static final String INVALID_LOTTO_NUMBER_FORMAT_MESSAGE = "당첨 번호의 형식이 잘못 되었습니다. 예) 1, 2, 3, 4, 5, 6";
     static final String INVALID_BONUS_NUMBER_FORMAT_MESSAGE = "보너스 번호의 형식이 잘못 되었습니다. 예) 35";
     static final String INVALID_MONEY_FORMAT_MESSAGE = "금액은 1000의 배수여야 합니다. 예) 10000";
@@ -22,20 +24,12 @@ public class StringFormatValidator {
 
     public static StringFormatValidator lottoValidator() {
         return new StringFormatValidator(
-            "^" + numberWithSpacesRegex() + "(\\s*," + numberWithSpacesRegex() + "){5}$",
+            "^" + NUMBER_WITH_SPACE_REGEX + "(\\s*," + NUMBER_WITH_SPACE_REGEX + "){5}$",
             INVALID_LOTTO_NUMBER_FORMAT_MESSAGE);
     }
 
-    private static String numberWithSpacesRegex() {
-        return "\\s*" + numberRegex() + "\\s*";
-    }
-
-    private static String numberRegex() {
-        return "[1-9][0-9]*";
-    }
-
     public static StringFormatValidator numberValidator() {
-        return new StringFormatValidator(numberWithSpacesRegex(),
+        return new StringFormatValidator(NUMBER_WITH_SPACE_REGEX,
             INVALID_BONUS_NUMBER_FORMAT_MESSAGE);
     }
 
